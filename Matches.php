db08a3d05523ec39a67e15b80b82abf76645e473
Vesa Beqa <vb73040@ubt-uni.net>
2026-02-01 19:56:10 +0100

    permirsim i vogel

--- Matches.php
+++ Matches.php
@@ -16,7 +16,7 @@ class Matches {
     }
 
     public function getAll() {
-        $stmt = $this->conn->query("SELECT * FROM matches ORDER BY match_date DESC");
+        $stmt = $this->conn->query("SELECT * FROM matches ORDER BY match_date ASC");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
